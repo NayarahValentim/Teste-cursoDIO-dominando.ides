048955b68785efc91ba456dd9965b9085ed6564e
NayarahValentim <nayarahsoufeliz@hotmail.com>
2022-07-14 18:44:15 -0300

    First commit

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,23 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroPrograma {
+	
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World " + (a + b));
+	}*/
+	}
+}
+	class Livros {
+		private String nome;
+		private String npag;
+	}
+		
+
