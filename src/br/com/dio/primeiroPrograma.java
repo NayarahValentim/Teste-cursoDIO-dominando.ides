692663aa200e87d79b1cabba33c75a7effea6651
NayarahValentim <nayarahsoufeliz@hotmail.com>
2022-07-16 16:56:58 -0300

    Correção programa

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -15,9 +15,38 @@ public class primeiroPrograma {
 	}*/
 	}
 }
-	class Livros {
-		private String nome;
-		private String npag;
-	}
+class  Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
 		
 
